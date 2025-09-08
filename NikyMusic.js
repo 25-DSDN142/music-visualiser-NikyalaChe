cc2ca6a44db9e9b0dd3077ee12e035fb8f796fa3
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-08 18:07:56 +1200

    trying out shapes
    tried out an ellipse(circle) that changes size according to the drum
    
    centering it in the middle, I am planning to make more circle rings around it that changes with it

--- NikyMusic.js
+++ NikyMusic.js
@@ -1,10 +1,19 @@
+let circleX = 910;
+let circleSize;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
-  
+  circleSize = drum*4; //drum
+
+ fill(112, 17, 74); //dark pink
+
+ ellipse(circleX,540,circleSize); 
  
 
-  
+
+ //circleX = circleX + 1;
+
+
 }
 
