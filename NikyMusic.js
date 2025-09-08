5de6f6987fe9aa7b322dc6a4f5a1a483284ee445
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-09 11:24:28 +1200

    tried out different number of rings and size
    tried out more rings and increasing the value that its times against the drums

--- NikyMusic.js
+++ NikyMusic.js
@@ -10,26 +10,31 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   //colourMode(HSB); 
 
   //console.log(drum);
-  circleSize = drum * 7; //drum
+  circleSize = drum * 10; //drum
 
   stroke(112, 17, 74); //dark purple ring
   strokeWeight(10);
   noFill();
 
-
 // fill(112, 17, 74); //dark pink
 
 //draw multiple rings within one another 
-let numRings = 6; //number of rings displayed inside oneanother
+let numRings = 10; //number of rings displayed inside oneanother
 let ringSpacing = 50; //space between
+
+let color1 = color(112, 17, 74); //dark purple
+let color2 = color(255, 0, 85); //dark pink
+
+let colorDriver = map(bass, 0, 100, 0, 1);
+
+let interColor = lerpColor(color1, color2, colorDriver); //middle colour between purple and pink
+fill(interColor)
+
 
 for (let i = 0; i < numRings; i++) {
   ellipse(circleX, 540, circleSize - i * ringSpacing);
 }
 
-
- //ellipse(circleX, 540, circleSize-50)
- //circleX = circleX + 1;
 
 
 }
