e9422f251a0ffa4d852088dc5260ce4e2c06748a
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-08 18:48:48 +1200

    making rings
    - changed the background to black
    - gave the ellipse a stroke weight and coloured it the pink colour I had originally made the ellipse
    - filled it black to match background

--- NikyMusic.js
+++ NikyMusic.js
@@ -3,14 +3,18 @@ let circleSize;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  circleSize = drum*4; //drum
+  background(0); //black backgound
+  circleSize = drum*7; //drum
 
- fill(112, 17, 74); //dark pink
+  stroke (112, 17, 74); //dark purple ring
+  strokeWeight(10);
+  fill(0, 0, 0); //black center
+
+// fill(112, 17, 74); //dark pink
 
  ellipse(circleX,540,circleSize); 
  
-
+ 
 
  //circleX = circleX + 1;
 
