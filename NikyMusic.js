de7004d8cd7439ce0c2fcea486ea92d71fb846f8
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-09 10:54:16 +1200

    tue 9 check

--- NikyMusic.js
+++ NikyMusic.js
@@ -6,19 +6,26 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(0); //black backgound
   //colourMode(HSB); 
 
-  circleSize = drum*7; //drum
+  //console.log(drum);
+  circleSize = drum * 7; //drum
 
-  stroke (112, 17, 74); //dark purple ring
+  stroke(112, 17, 74); //dark purple ring
   strokeWeight(10);
   noFill();
 
 
 // fill(112, 17, 74); //dark pink
 
- ellipse(circleX,540,circleSize); 
- ellipse(circleX, 540, circleSize-50)
- 
+//draw multiple rings within one another 
+let numRings = 6; //number of rings displayed inside oneanother
+let ringSpacing = 50; //space between
 
+for (let i = 0; i < numRings; i++) {
+  ellipse(circleX, 540, circleSize - i * ringSpacing);
+}
+
+
+ //ellipse(circleX, 540, circleSize-50)
  //circleX = circleX + 1;
 
 
