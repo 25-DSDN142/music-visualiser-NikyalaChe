40fecde0629611e323bb95a19b8ad03fb0fe0886
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-09 12:04:17 +1200

    tried making a star
    attempted a star but didnt work out how I wanted it according the copilot help so will try again with its own vertices etc

--- NikyMusic.js
+++ NikyMusic.js
@@ -22,20 +22,35 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 let numRings = 10; //number of rings displayed inside oneanother
 let ringSpacing = 50; //space between
 
-let color1 = color(112, 17, 74); //dark purple
-let color2 = color(255, 0, 85); //dark pink
+let ring1 = color(112, 17, 74); //dark purple
+let ring2 = color(255, 0, 85); //dark pink
 
 let colorDriver = map(bass, 0, 100, 0, 1);
 
-let interColor = lerpColor(color1, color2, colorDriver); //middle colour between purple and pink
+let interColor = lerpColor(ring1, ring2, colorDriver); //middle colour between purple and pink
 fill(interColor)
 
 
 for (let i = 0; i < numRings; i++) {
   ellipse(circleX, 540, circleSize - i * ringSpacing);
 }
+ let numPoints = 10; //number of points for the star
+//draw star around the rings
+beginShape();
+stroke(166, 227, 180); //light neon green
+strokeWeight(5);
+noFill();
 
+//copilot helped me here 
+for (let i = 0; i < numPoints; i++) {
+  let angle = map(i, 0, numPoints, 0, TWO_PI);
+  let x = circleX + cos(angle) * (circleSize / 2 + 10);
+  let y = 540 + sin(angle) * (circleSize / 2 + 10);
+  vertex(x, y);
+}
+endShape(CLOSE);
 
+//copilot code ends here
 
 }
 
