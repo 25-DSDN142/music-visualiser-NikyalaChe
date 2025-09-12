441ef93c48dcf029c5f4c35809a49b672ed5b131
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-13 02:13:11 +1200

    circle in corner
    made the circle in the corner and will do this in all 4 corners before playing around with different colors

--- practiceCode.js
+++ practiceCode.js
@@ -1,5 +1,6 @@
 
 let circleX = 910;
+let circleX2 = 150;
 let circleSize;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
@@ -47,12 +48,12 @@ for (let i = 0; i < numRings; i++) {
 
 }
 
-let numRings2 = 5; //numbers of rings for corner circle
-let ringSpacing2 = 100; //space between 
+let numRings2 = 10; //numbers of rings for corner circle
+let ringSpacing2 = 50; //space between 
 
 //draw circle in top left corner
 for (let i = 0; i < numRings2; i++) {
-  ellipse(150, 150, circleSize - i * ringSpacing2);
+  ellipse(circleX2, circleX2, circleSize - i * ringSpacing2);
 
 }
 
