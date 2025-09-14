0e12d851d2e8038d37ff0428a39bb2baeaee24ae
NikyalaChe <symonsniky@myvuw.ac.nz>
2025-09-15 10:30:44 +1200

    trying out lines from different parts of page
    adding some lines that follow the y axis up and down and along the xaxis
    - evenly spaced and made to be only half way down the page, i am thinking of adding some coming from the bottom upwards
    - going to try having these lines respond to either the words, bass or vocals

--- starfield.js
+++ starfield.js
@@ -78,91 +78,101 @@ ellipse(sx, sy, r, r);
 }
 pop();
 
+//lines from top to bottom yaxis
+let numVerticalLines = 20;
+let spacing = width / (numVerticalLines + 1);
+
+strokeWeight(5);
+stroke(255);
+
+for (let i = 1; i <= numVerticalLines; i++) {
+let x = spacing * i;
+line(x, 0, x, height / 2); //top to bottom
 
 
 
-//MIDDLE CIRCLE
-  console.log(drum);
-  circleSize = drum * 10; //drum
-  
-
-//RINGS IN THE MIDDLE
-  stroke(112, 17, 74); //dark purple ring
-  strokeWeight(10);
-  noFill();
-
-// fill(112, 17, 74); //dark pink
-
-//switch to RGB for the rings
-colorMode(RGB, 255, 255, 255);
-
-//color changing with bass
-let ring1 = color(112, 17, 74); //dark purple
-//let ring2 = color(255, 0, 85); //dark pink
-let ring2 = color(166, 227, 180); //light neon green
-let colorDriver = map(bass, 0, 100, 0, 1);
-let interColor = lerpColor(ring1, ring2, colorDriver); //middle colour between purple and pink
-
-fill(interColor)
-stroke(ring1);
-
-//draw multiple rings within one another 
-let numRings = 10; //number of rings displayed inside oneanother
-let ringSpacing = 50; //space between
-
-for (let i = 0; i < numRings; i++) {
-  ellipse(circleX, 540, circleSize - i * ringSpacing);
-
-
+////MIDDLE CIRCLE
+//  console.log(drum);
+//  circleSize = drum * 10; //drum
+//  
+//
+////RINGS IN THE MIDDLE
+//  stroke(112, 17, 74); //dark purple ring
+//  strokeWeight(10);
+//  noFill();
+//
+//// fill(112, 17, 74); //dark pink
+//
+////switch to RGB for the rings
+//colorMode(RGB, 255, 255, 255);
+//
+////color changing with bass
+//let ring1 = color(112, 17, 74); //dark purple
+////let ring2 = color(255, 0, 85); //dark pink
+//let ring2 = color(166, 227, 180); //light neon green
+//let colorDriver = map(bass, 0, 100, 0, 1);
+//let interColor = lerpColor(ring1, ring2, colorDriver); //middle colour between purple and pink
+//
+//fill(interColor)
+//stroke(ring1);
+//
+////draw multiple rings within one another 
+//let numRings = 10; //number of rings displayed inside oneanother
+//let ringSpacing = 50; //space between
+//
+//for (let i = 0; i < numRings; i++) {
+//  ellipse(circleX, 540, circleSize - i * ringSpacing);
+//
+//
+//}
+//
+//// CORNER CIRCLES
+//if (drum > 82) { 
+//  circleSize2 = drum * 8;
+//
+////switch to RGB for the rings
+//colorMode(RGB, 255, 255, 255);
+//
+//
+////color changing with bass
+////let ring1 = color(255,255,254); //white 
+//let ring1 = color(255, 0, 85); //warm pink 
+//let ring2 = color(166, 220, 227); //light blue
+////let ring2 = color(245, 155, 72); //light orange
+//
+//let colorDriver = map(bass, 0, 100, 0, 1);
+//let interColor = lerpColor(ring1, ring2, colorDriver); //middle colour between purple and pink
+//
+//fill(interColor)
+//stroke(ring1);
+//
+//let numRings2 = 20; //numbers of rings for corner circle
+//let ringSpacing2 = 40; //space between 
+//
+////Ai helped me create circles in each corner
+//// Array of corner positions: [x, y]
+//let corners = [
+//  [circleX2, circleX2], // top-left
+//  [width - circleX2, circleX2], // top-right
+//  [circleX2, height - circleX2], // bottom-left
+//  [width - circleX2, height - circleX2] // bottom-right
+//];
+//
+//// Draw rings in all four corners
+//for (let c = 0; c < corners.length; c++) {
+//  let x = corners[c][0];
+//  let y = corners[c][1];
+//  for (let i = 0; i < numRings2; i++) {
+//    ellipse(x, y, circleSize2 - i * ringSpacing2);
+//  }
+////ai code ended
+//
+////draw circle in top left corner //code used to make one circle in top left corner
+////for (let i = 0; i < numRings2; i++) {
+////  ellipse(circleX2, circleX2, circleSize - i * ringSpacing2);
+//  }
+// }
+//
+//
 }
-
-// CORNER CIRCLES
-if (drum > 82) { 
-  circleSize2 = drum * 8;
-
-//switch to RGB for the rings
-colorMode(RGB, 255, 255, 255);
-
-
-//color changing with bass
-//let ring1 = color(255,255,254); //white 
-let ring1 = color(255, 0, 85); //warm pink 
-let ring2 = color(166, 220, 227); //light blue
-//let ring2 = color(245, 155, 72); //light orange
-
-let colorDriver = map(bass, 0, 100, 0, 1);
-let interColor = lerpColor(ring1, ring2, colorDriver); //middle colour between purple and pink
-
-fill(interColor)
-stroke(ring1);
-
-let numRings2 = 20; //numbers of rings for corner circle
-let ringSpacing2 = 40; //space between 
-
-//Ai helped me create circles in each corner
-// Array of corner positions: [x, y]
-let corners = [
-  [circleX2, circleX2], // top-left
-  [width - circleX2, circleX2], // top-right
-  [circleX2, height - circleX2], // bottom-left
-  [width - circleX2, height - circleX2] // bottom-right
-];
-
-// Draw rings in all four corners
-for (let c = 0; c < corners.length; c++) {
-  let x = corners[c][0];
-  let y = corners[c][1];
-  for (let i = 0; i < numRings2; i++) {
-    ellipse(x, y, circleSize2 - i * ringSpacing2);
-  }
-//ai code ended
-
-//draw circle in top left corner //code used to make one circle in top left corner
-//for (let i = 0; i < numRings2; i++) {
-//  ellipse(circleX2, circleX2, circleSize - i * ringSpacing2);
-  }
- }
-
-
 }
-
